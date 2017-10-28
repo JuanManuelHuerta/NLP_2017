58b17a24e6b9edcf23d8628fee9c74742fc34401
Juan M Huerta <jmhuertany@gmail.com>
2017-10-28 16:06:16 -0400

    Updates to third class

--- class_3/l03s02.py
+++ class_3/l03s02.py
@@ -17,8 +17,10 @@ def custom_word_tokenize(my_string):
     words=s3.split(" ")
     return words
 
-
-
+def custom_similarity(a,b):
+    i=len(a.intersection(b))
+    d=float(i)/(len(a)+len(b)-i)
+    return d
 
 ## Open the file, scan the data, keep review texts.
 
@@ -36,7 +38,7 @@ fp=gzip.open("../reviews_Beauty_5.json.gz")
 master_dictionary={}
 dictionary_per_product={}
 product_count={}
-
+words_per_product={}
 for line in fp:
     review_data=json.loads(line)
     asin=review_data['asin']
@@ -53,7 +55,7 @@ for line in fp:
     review=unicodedata.normalize('NFKD', review_data['reviewText']).encode('ascii','ignore')
     if not asin in dictionary_per_product:
         dictionary_per_product[asin]={}
-
+        words_per_product[asin]=0.0
     words=nltk.word_tokenize(review)    
 
     for word_r in words:
@@ -65,7 +67,7 @@ for line in fp:
                 dictionary_per_product[asin][word]=0
             master_dictionary[word]+=1
             dictionary_per_product[asin][word]+=1
-
+            words_per_product[asin]+=1.0
 
 top_words={}
 InverseDocumentFrequency={}
@@ -82,17 +84,33 @@ for asin in dictionary_per_product:
     top_words[asin]=set()
 
     for word in  dictionary_per_product[asin]:
-        dictionary_per_product[asin][word]=dictionary_per_product[asin][word]*math.log(len(dictionary_per_product)/InverseDocumentFrequency[word])
+        if dictionary_per_product[asin][word]>=10.0:
 
-    dpp_sorted=sorted(dictionary_per_product[asin].items(),key=operator.itemgetter(1), reverse=True)
-
-    for word in dpp_sorted:
-        top_words[asin].add(word)
-        if len(top_words[asin])>30:
-            break
-
-    print asin, top_words[asin]
-        
+            tf_idf=((float(dictionary_per_product[asin][word])/words_per_product[asin])*math.log(len(dictionary_per_product)/InverseDocumentFrequency[word]))
+            if tf_idf>=0.0001:
+                #top_words[asin].add((word,tf_idf))
+                top_words[asin].add(word)
 
 
+#    print asin, top_words[asin]
+nodes=set()        
+similarity_graph={}
+fo1=open("links.csv","wt")
+fo2=open("nodes.csv","wt")
+fo1.write("source,target,type\n")
+fo2.write("node,group\n")
 
+for asin1 in dictionary_per_product:
+    similarity_graph[asin1]={}
+    for asin2 in dictionary_per_product:
+        if asin1<asin2:
+            d=custom_similarity(top_words[asin1],top_words[asin2])
+            if d>0.44:
+                similarity_graph[asin1][asin2]=d
+                fo1.write( asin1+","+ asin2+",type1\n")
+                nodes.add(asin1)
+                nodes.add(asin2)
+            
+
+for node in nodes:
+    fo2.write(node+",group1\n")
